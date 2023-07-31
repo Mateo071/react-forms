723b4845a7e9bfd95a732e41fde28de147a95bd1
Mateo071 <floresmateo2000@gmail.com>
2023-07-31 11:44:07 -0400

    completed user and password authentication

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,15 +1,40 @@
 import {useState} from 'react'
 
+const API_URL = `https://fsa-jwt-practice.herokuapp.com/signup`;
+
 const SignUpForm = () => {
 
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
 
+  const handleSubmit = async(event) => {
+    event.preventDefault();
+    
+    try {
+      const response = await fetch(API_URL, {
+        method: "POST",
+        body: JSON.stringify({
+          username,
+          password,
+        }),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+      const data = await response.json();
+      console.log(data);
+    } catch(err) {
+      console.err(err);
+      setError(err.message);
+    }
+  }
+
   return (
     <>
     <h2>Sign Up</h2>
-    <form>
+    {error && <p>Error: {error}</p>}
+    <form onSubmit={handleSubmit}>
       <label>Username: <input type="text" value={username} onChange={(event) => setUsername(event.target.value)} /></label>
       <label>Password: <input type="password" value={password} onChange={(event) => setPassword(event.target.value)} /></label>
       <button type="submit">Submit</button>
