2a9ffa845605530662832b82b26a3262d2813660
Mateo071 <floresmateo2000@gmail.com>
2023-07-31 11:28:39 -0400

    added username and password inputs

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,6 +1,20 @@
+import {useState} from 'react'
+
 const SignUpForm = () => {
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
   return (
+    <>
     <h2>Sign Up</h2>
+    <form>
+      <label>Username: <input type="text" value={username} onChange={(event) => setUsername(event.target.value)} /></label>
+      <label>Password: <input type="password" value={password} onChange={(event) => setPassword(event.target.value)} /></label>
+      <button type="submit">Submit</button>
+    </form>
+    </>
   )
 }
 
